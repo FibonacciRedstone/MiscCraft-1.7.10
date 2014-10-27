216575395443ebdbb0a8d1852239f6ba9213634e
Fibonacci <fibonacci336@gmail.com>
2014-10-26 18:04:58 -0600

    Added Source Code

--- main/java/com/fibonacci/MiscCraft/block/tileentity/TileEntityMobSpawnBlocker.java
+++ main/java/com/fibonacci/MiscCraft/block/tileentity/TileEntityMobSpawnBlocker.java
@@ -4,20 +4,41 @@ import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 
+import java.util.List;
+
 /**
  * Created by Fibonacci on 10/26/14.
  */
 public class TileEntityMobSpawnBlocker extends TileEntity {
-    private AxisAlignedBB boundingbox;
-    private EntityMob entity;
+    private AxisAlignedBB boundingbox = AxisAlignedBB.getBoundingBox(-3.0D, 0.0D, -3.0D, 3.0D, 3.0D, 3.0D);
+    ;
+
+    //private EntityMob mob;
+//    private double differenceX = mob.posX - this.xCoord;
+//    private double differenceY = mob.posY - this.yCoord;
+//    private double differenceZ = mob.posZ - this.zCoord;
+
+
     public TileEntityMobSpawnBlocker() {
-        this.boundingbox = AxisAlignedBB.getBoundingBox(-3.0D, 0.0D, -3.0D, 3.0D, 3.0D, 3.0D);
-        entity = (EntityMob) worldObj.getEntitiesWithinAABB(EntityMob.class, boundingbox);
+
     }
 
     public void updateEntity(){
         if(worldObj.isBlockIndirectlyGettingPowered(xCoord, yCoord, zCoord)){
-            System.out.println(entity);
+//            if(differenceX <= 3 || differenceX >= -3){
+//                worldObj.removeEntity(mob);
+//            }
+//            if(differenceY <= 3 || differenceX >= -3){
+//                worldObj.removeEntity(mob);
+//            }
+//            if(differenceZ <= 3 || differenceX >= -3){
+//                worldObj.removeEntity(mob);
+//            }
+            //System.out.println(getEntity());
+
         }
+    }
+    public List<EntityMob> getEntity(){
+        return worldObj.getEntitiesWithinAABB(EntityMob.class, boundingbox);
     }
 }
